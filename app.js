40c29145d27927725e5e65487f8fcaba9a50c231
Regis <regisuniverso@gmail.com>
2023-12-22 19:36:58 -0300

    Modificando os arquivo

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ alert('Boas vindas ao jogo do número secreto!!!');
 
 let numeroSecreto = 5;
 
-let chute = prompt('Escolha um número entre 1 e 30:');
+console.log(numeroSecreto);
+
+// let chute = prompt('Escolha um número entre 1 e 30:');
 /*a função prompt() é usada para exibir uma caixa de diálogo ao usuário, permitindo que ele insira dados de texto. E a variável 'chute' RECEBE os dados digitados no Prompt, pelo usuário.*/
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
-} else {
-    alert('Você errou :(')
+let chute;
+let tentativas = 1;
+
+// ENQUANTO chute NÃO for igual ao numeroSecreto
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 30:');
+  // se chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}:)com 
+    ${tentativas} tentativas`);
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é MENOR que  
+${chute}`);
+  } else {
+    alert(`O número secreto é MAIOR que  
+${chute}`);
+  }
+  // tentativas = tentativas + 1;
+  tentativas++;
 }
-
-
-
-
-
-
