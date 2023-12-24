7aa17363b92e7609efd28e199f9d668132f760fd
Regis <regisuniverso@gmail.com>
2023-12-24 10:53:02 -0300

    midificando app.js

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-alert('Boas vindas ao jogo do número secreto!!!');
-/*A função alert() em JavaScript é usada para exibir uma caixa de diálogo de alerta ao usuário. */
+alert('Boas vindas ao jogo do número secreto!!!')
+//*A função alert() em JavaScript é usada para exibir uma caixa de diálogo de alerta ao usuário. */
 
-let numeroSecreto = 5;
+// Function para GERAR UM NÚMERO ALEATÓRIO entre  1 e 100
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 
-console.log(numeroSecreto);
+console.log(numeroSecreto)
 
 // let chute = prompt('Escolha um número entre 1 e 30:');
 /*a função prompt() é usada para exibir uma caixa de diálogo ao usuário, permitindo que ele insira dados de texto. E a variável 'chute' RECEBE os dados digitados no Prompt, pelo usuário.*/
 
-let chute;
-let tentativas = 1;
+let chute
+let tentativas = 1
 
 // ENQUANTO chute NÃO for igual ao numeroSecreto
 while (chute != numeroSecreto) {
-  chute = prompt('Escolha um número entre 1 e 30:');
+  chute = prompt('Escolha um número entre 1 e 100:')
   // se chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}:)com 
-    ${tentativas} tentativas`);
+    break
   } else if (chute > numeroSecreto) {
     alert(`O número secreto é MENOR que  
-${chute}`);
+${chute}`)
   } else {
     alert(`O número secreto é MAIOR que  
-${chute}`);
+${chute}`)
   }
   // tentativas = tentativas + 1;
-  tentativas++;
+  tentativas++
 }
+
+//  OPERADOR TERNÁRIO
+let palavraTentativa = tentativas > 1 ? 'tentativaas' : 'tentativa';
+alert(`Isso ai! Você ACERRTOU!!! ${numeroSecreto} é o NÚMERO SECRETO - Você precisou de       
+ ${tentativas} ${palavraTentativa}`)
+
+// let palavraTentativa ACIMA, SUBSTITUI o if, ABAIXO
+
+// if (tentativas > 1) {
+//   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com
+// ${tentativas} tentativas`);
+// } else {
+//   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com
+// ${tentativas} tentativa`);
+// }
